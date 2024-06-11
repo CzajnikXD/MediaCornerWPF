44fcba934506cd2ec2a438bfede1c2f459b8a859
CzajnikXD <264275@student.pwr.edu.pl>
2024-06-11 22:44:45 +0200

    Dodano wszystkie Widoki i Modele dla ChildViews, Dodano MainMenuWindowModel i dodano wszystkie childviews do MainMenuWindow

---
+++ MediaCornerWPF/ViewModels/MainMenuWindowModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using FontAwesome.Sharp;
+using System.Windows.Input;
+
+namespace MediaCornerWPF.ViewModels
+{
+    public class MainMenuWindowModel : ViewModelBase
+    {
+        private ViewModelBase _currentChildView;
+        private string _caption;
+        private IconChar _icon;
+
+        public ViewModelBase CurrentChildView
+        {
+            get
+            {
+                return _currentChildView;
+            }
+            set
+            {
+                _currentChildView = value;
+                OnPropertyChanged(nameof(CurrentChildView));
+            }
+        }
+
+        public string Caption
+        {
+            get
+            {
+                return _caption;
+            }
+            set
+            {
+                _caption = value;
+                OnPropertyChanged(nameof(Caption));
+            }
+        }
+
+        public IconChar Icon
+        {
+            get
+            {
+                return _icon;
+            }
+            set
+            {
+                _icon = value;
+                OnPropertyChanged(nameof(Icon));
+            }
+        }
+
+        //---> Commands
+        public ICommand ShowHomeViewCommand { get; }
+        public ICommand ShowWatchlistViewCommand { get; }
+        public ICommand ShowMovieViewCommand { get; }
+        public ICommand ShowUsersViewCommand { get; }
+        public ICommand ShowSettingsViewCommand { get; }
+
+        public MainMenuWindowModel()
+        {
+            //Initialize commands
+            ShowHomeViewCommand = new ViewModelCommand(ExecuteShowHomeViewCommand);
+            ShowWatchlistViewCommand = new ViewModelCommand(ExecuteShowWatchlistCommand);
+            ShowMovieViewCommand = new ViewModelCommand(ExecuteShowMovieViewCommand);
+            ShowUsersViewCommand = new ViewModelCommand(ExecuteShowUsersViewCommand);
+            ShowSettingsViewCommand = new ViewModelCommand(ExecuteShowSettingsViewCommand);
+
+            //Default view
+            ExecuteShowHomeViewCommand(null);
+        }
+
+        private void ExecuteShowHomeViewCommand(object obj)
+        {
+            CurrentChildView = new HomeViewModel();
+            Caption = "Dashboard";
+            Icon = IconChar.Home;
+        }
+
+        private void ExecuteShowWatchlistCommand(object obj)
+        {
+            CurrentChildView = new WatchlistViewModel();
+            Caption = "Watchlist";
+            Icon = IconChar.Tv;
+        }
+
+        private void ExecuteShowMovieViewCommand(object obj)
+        {
+            CurrentChildView = new MovieViewModel();
+            Caption = "Movie";
+            Icon = IconChar.Film;
+        }
+
+        private void ExecuteShowUsersViewCommand(object obj)
+        {
+            CurrentChildView = new UsersViewModel();
+            Caption = "Users";
+            Icon = IconChar.UserGroup;
+        }
+
+        private void ExecuteShowSettingsViewCommand(object obj)
+        {
+            CurrentChildView = new SettingsViewModel();
+            Caption = "Settings";
+            Icon = IconChar.Tools;
+        }
+    }
+}
---
+++ MediaCornerWPF/ViewModels/WatchlistViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaCornerWPF.ViewModels
+{
+    public class WatchlistViewModel : ViewModelBase
+    {
+    }
+}
