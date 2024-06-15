b87a3c5922d3cdd231831046f6c6b105f37faf0e
CzajnikXD <264275@student.pwr.edu.pl>
2024-06-15 17:03:45 +0200

    dodano komentarze do bazy

--- MediaCornerWPF/Lib/API/Calls/MovieController.cs
+++ MediaCornerWPF/Lib/API/Calls/MovieController.cs
@@ -7,6 +7,15 @@ namespace MediaCornerWPF.Lib.API.Calls
 {
     internal class MovieController
     {
+        /// <summary>
+        /// Funkcja pobierająca listę popularnych filmów z The Movie Database API.
+        /// </summary>
+        /// <returns>
+        ///     Lista obiektów MovieModel reprezentujących popularne filmy.
+        /// </returns>
+        /// <exception cref="Exception">
+        ///     Rzuca wyjątek w przypadku nieudanej próby pobrania danych.
+        /// </exception>
         public static async Task<List<MovieModel>> GetPopular()
         {
             var request = new HttpRequestMessage
@@ -14,10 +23,10 @@ namespace MediaCornerWPF.Lib.API.Calls
                 Method = HttpMethod.Get,
                 RequestUri = new Uri("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1"),
                 Headers =
-    {
-        { "accept", "application/json" },
-        { "Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzVhMjY4ZDkxZDk1YjczOGFkODEyOGY2NTNjMzNmNSIsInN1YiI6IjY2NjQ2NmUyODYwMjJkOWQ5OTc4Yzg4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPwar435YWATMewdq9Fwm_crr_oRjjV9aB69ZwFL7VY" },
-    },
+                {
+                    { "accept", "application/json" },
+                    { "Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzVhMjY4ZDkxZDk1YjczOGFkODEyOGY2NTNjMzNmNSIsInN1YiI6IjY2NjQ2NmUyODYwMjJkOWQ5OTc4Yzg4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPwar435YWATMewdq9Fwm_crr_oRjjV9aB69ZwFL7VY" },
+                },
             };
 
             using (var response = await ApiController.ApiClient.SendAsync(request))
@@ -39,6 +48,18 @@ namespace MediaCornerWPF.Lib.API.Calls
             }
         }
 
+        /// <summary>
+        /// Funkcja pobierająca szczegóły filmu na podstawie jego identyfikatora z The Movie Database API.
+        /// </summary>
+        /// <param name="id">
+        ///     Identyfikator filmu, którego szczegóły mają zostać pobrane.
+        /// </param>
+        /// <returns>
+        ///     Obiekt MovieModel reprezentujący szczegóły filmu.
+        /// </returns>
+        /// <exception cref="Exception">
+        ///     Rzuca wyjątek w przypadku nieudanej próby pobrania danych.
+        /// </exception>
         public static async Task<MovieModel> GetMovie(int id)
         {
             var request = new HttpRequestMessage
@@ -46,10 +67,10 @@ namespace MediaCornerWPF.Lib.API.Calls
                 Method = HttpMethod.Get,
                 RequestUri = new Uri($"https://api.themoviedb.org/3/movie/{id}?language=en-US"),
                 Headers =
-    {
-        { "accept", "application/json" },
-        { "Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzVhMjY4ZDkxZDk1YjczOGFkODEyOGY2NTNjMzNmNSIsInN1YiI6IjY2NjQ2NmUyODYwMjJkOWQ5OTc4Yzg4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPwar435YWATMewdq9Fwm_crr_oRjjV9aB69ZwFL7VY" },
-    },
+                {
+                    { "accept", "application/json" },
+                    { "Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzVhMjY4ZDkxZDk1YjczOGFkODEyOGY2NTNjMzNmNSIsInN1YiI6IjY2NjQ2NmUyODYwMjJkOWQ5OTc4Yzg4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPwar435YWATMewdq9Fwm_crr_oRjjV9aB69ZwFL7VY" },
+                },
             };
 
             using (var response = await ApiController.ApiClient.SendAsync(request))
@@ -69,6 +90,18 @@ namespace MediaCornerWPF.Lib.API.Calls
             }
         }
 
+        /// <summary>
+        /// Funkcja wyszukująca filmy na podstawie podanego zapytania w The Movie Database API.
+        /// </summary>
+        /// <param name="query">
+        ///     Zapytanie wyszukiwane.
+        /// </param>
+        /// <returns>
+        ///     Lista obiektów MovieModel reprezentujących wyniki wyszukiwania.
+        /// </returns>
+        /// <exception cref="Exception">
+        ///     Rzuca wyjątek w przypadku nieudanej próby pobrania danych.
+        /// </exception>
         public static async Task<List<MovieModel>> SearchMovie(string query)
         {
             var request = new HttpRequestMessage
@@ -76,10 +109,10 @@ namespace MediaCornerWPF.Lib.API.Calls
                 Method = HttpMethod.Get,
                 RequestUri = new Uri($"https://api.themoviedb.org/3/search/movie?query={query}&include_adult=false&language=en-US&page=1"),
                 Headers =
-    {
-        { "accept", "application/json" },
-        { "Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzVhMjY4ZDkxZDk1YjczOGFkODEyOGY2NTNjMzNmNSIsInN1YiI6IjY2NjQ2NmUyODYwMjJkOWQ5OTc4Yzg4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPwar435YWATMewdq9Fwm_crr_oRjjV9aB69ZwFL7VY" },
-    },
+                {
+                    { "accept", "application/json" },
+                    { "Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzVhMjY4ZDkxZDk1YjczOGFkODEyOGY2NTNjMzNmNSIsInN1YiI6IjY2NjQ2NmUyODYwMjJkOWQ5OTc4Yzg4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPwar435YWATMewdq9Fwm_crr_oRjjV9aB69ZwFL7VY" },
+                },
             };
 
             using (var response = await ApiController.ApiClient.SendAsync(request))
@@ -102,4 +135,3 @@ namespace MediaCornerWPF.Lib.API.Calls
         }
     }
 }
-
--- MediaCornerWPF/Lib/MongoDB/DB.cs
+++ MediaCornerWPF/Lib/MongoDB/DB.cs
@@ -3,7 +3,6 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 using System.Diagnostics;
 
-
 namespace MediaCornerWPF.Lib.MongoDB
 {
     public class DB
@@ -14,11 +13,14 @@ namespace MediaCornerWPF.Lib.MongoDB
         public static IMongoCollection<UsersModel> UsersCollection { get; set; }
         public static IMongoCollection<WatchlistedModel> WatchlistCollection { get; set; }
 
+        /// <summary>
+        /// Funkcja inicjalizująca połączenie z bazą danych MongoDB.
+        /// </summary>
         public static void InitDB()
         {
             Debug.WriteLine("DB INIT");
 
-           ConnectionString = "mongodb+srv://admin:ZAQ123wsx@projects.gkf3nwl.mongodb.net/";
+            ConnectionString = "mongodb+srv://admin:ZAQ123wsx@projects.gkf3nwl.mongodb.net/";
 
             var settings = MongoClientSettings.FromConnectionString(ConnectionString);
             settings.ServerApi = new ServerApi(ServerApiVersion.V1);
@@ -29,6 +31,19 @@ namespace MediaCornerWPF.Lib.MongoDB
             WatchlistCollection = DbName.GetCollection<WatchlistedModel>("Watchlist");
         }
 
+        /// <summary>
+        /// Funkcja autoryzująca użytkownika na podstawie nazwy użytkownika i hasła.
+        /// </summary>
+        /// <param name="username">
+        ///     Nazwa użytkownika do autoryzacji.
+        /// </param>
+        /// <param name="password">
+        ///     Hasło użytkownika do autoryzacji.
+        /// </param>
+        /// <returns>
+        ///     True - jeśli autoryzacja się powiodła.
+        ///     False - jeśli autoryzacja się nie powiodła.
+        /// </returns>
         public static bool AuthorizeUser(string username, string password)
         {
             var user = UsersCollection.Find(x => x.username == username && x.password == password).FirstOrDefault();
@@ -41,6 +56,15 @@ namespace MediaCornerWPF.Lib.MongoDB
             return false;
         }
 
+        /// <summary>
+        /// Funkcja pobierająca nazwę użytkownika na podstawie identyfikatora użytkownika.
+        /// </summary>
+        /// <param name="UserId">
+        ///     Identyfikator użytkownika.
+        /// </param>
+        /// <returns>
+        ///     Nazwa użytkownika.
+        /// </returns>
         public static string GetUsername(string UserId)
         {
             var user = UsersCollection.Find(x => x._id.ToString() == UserId).FirstOrDefault();
@@ -48,11 +72,21 @@ namespace MediaCornerWPF.Lib.MongoDB
             return user.username;
         }
 
+        /// <summary>
+        /// Funkcja dodająca film do listy "do obejrzenia" użytkownika.
+        /// </summary>
+        /// <param name="userId">
+        ///     Identyfikator użytkownika.
+        /// </param>
+        /// <param name="movieId">
+        ///     Identyfikator filmu.
+        /// </param>
         public static void AddToWatchlist(string userId, int movieId)
         {
             var alreadyWatchlisted = WatchlistCollection.Find(x => x.UsersId == userId && x.MovieId == movieId).FirstOrDefault();
 
-            if (alreadyWatchlisted != null) {
+            if (alreadyWatchlisted != null)
+            {
                 return;
             }
 
@@ -65,6 +99,15 @@ namespace MediaCornerWPF.Lib.MongoDB
             WatchlistCollection.InsertOne(watchlist);
         }
 
+        /// <summary>
+        /// Funkcja usuwająca film z listy "do obejrzenia" użytkownika.
+        /// </summary>
+        /// <param name="userId">
+        ///     Identyfikator użytkownika.
+        /// </param>
+        /// <param name="movieId">
+        ///     Identyfikator filmu.
+        /// </param>
         public static void RemoveFromWatchlist(string userId, int movieId)
         {
             var watchlist = WatchlistCollection.Find(x => x.UsersId == userId && x.MovieId == movieId).FirstOrDefault();
@@ -77,6 +120,15 @@ namespace MediaCornerWPF.Lib.MongoDB
             WatchlistCollection.DeleteOne(x => x.UsersId == userId && x.MovieId == movieId);
         }
 
+        /// <summary>
+        /// Funkcja pobierająca listę "do obejrzenia" użytkownika.
+        /// </summary>
+        /// <param name="userId">
+        ///     Identyfikator użytkownika.
+        /// </param>
+        /// <returns>
+        ///     Lista obiektów WatchlistedModel reprezentujących filmy do obejrzenia.
+        /// </returns>
         public static List<WatchlistedModel> GetWatchlist(string userId)
         {
             var watchlist = WatchlistCollection.Find(x => x.UsersId == userId).ToList();
@@ -84,6 +136,15 @@ namespace MediaCornerWPF.Lib.MongoDB
             return watchlist;
         }
 
+        /// <summary>
+        /// Funkcja pobierająca listę "do obejrzenia" innych użytkowników.
+        /// </summary>
+        /// <param name="userId">
+        ///     Identyfikator aktualnego użytkownika.
+        /// </param>
+        /// <returns>
+        ///     Lista obiektów WatchlistedModel reprezentujących filmy do obejrzenia innych użytkowników.
+        /// </returns>
         public static List<WatchlistedModel> GetOthersWatchlist(string userId)
         {
             var watchlist = WatchlistCollection.Find(x => x.UsersId != userId).ToList();
